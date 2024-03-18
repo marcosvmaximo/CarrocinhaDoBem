28dc79c650e21df50b7c2a30279a464dfacd1b51
vinicosper <cospervini@gmail.com>
2024-03-17 22:05:11 -0300

    meu commit

---
+++ demo vine/demo/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PetCadastroComponent } from './pages/pet-cadastro/pet-cadastro.component';
+import { PetComponent } from './pages/pet/pet.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: '#', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
+  { path: 'pet', component: PetComponent},
+  { path: 'pet-cadastro', component: PetCadastroComponent},
+  { path: 'cadastro', component: CadastroComponent}
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
