1d1f4b0af6f938debd4259e8c13bbf2366cbba67
vini-vg <vini02via@mail.com>
2024-03-20 16:48:30 -0300

    Implementação da tela de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { PetCadastroComponent } from './pages/pet-cadastro/pet-cadastro.component';
 import { PetComponent } from './pages/pet/pet.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
@@ -11,7 +12,8 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'pet', component: PetComponent},
   { path: 'pet-cadastro', component: PetCadastroComponent},
-  { path: 'cadastro', component: CadastroComponent}
+  { path: 'cadastro', component: CadastroComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
+import { delay, of } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,69 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  signInForm: FormGroup;
+  listaErros: string[] = [];
+
+  constructor(private formBuilder: FormBuilder) {
+    this.signInForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email, Validators.maxLength(100)]],
+      senha: ['', [Validators.required, this.validarSenha, Validators.maxLength(100)]]
+    });
+  }
+  
+  validarSenha(control: AbstractControl): { [key: string]: boolean } | null {
+    const senha = control.value;
+    const possuiLetraMaiuscula = /[A-Z]/.test(senha);
+    const possuiNumero = /[0-9]/.test(senha);
+    if (!possuiLetraMaiuscula || !possuiNumero) {
+      return { senhaInvalida: true };
+    }
+    return null;
+  }
+
+  onSubmit() {
+    this.listaErros = [];
+
+    Object.keys(this.signInForm.controls).forEach(campo => {
+      const control = this.signInForm.get(campo);
+
+      // @ts-ignore
+      if (control.invalid) {
+        // @ts-ignore
+        Object.keys(control.errors).forEach(erro => {
+          switch (erro) {
+            case 'required':
+              this.listaErros.push(`O campo ${campo} é obrigatório.`);
+              break;
+            case 'maxlength':
+              this.listaErros.push(`O campo ${campo} excedeu o número máximo de caracteres permitidos.`);
+              break;
+            case 'email':
+              this.listaErros.push(`O campo ${campo} deve ser um e-mail válido.`);
+              break;
+            case 'senhaCurta':
+              this.listaErros.push(`A senha deve ter no mínimo 8 caracteres.`);
+              break;
+            // Adicione outros casos de erro conforme necessário
+          }
+        });
+      }
+    });
+
+    this.esconder();
+  }
+
+  esconder(): void{
+    const timer = of(null).pipe(delay(5000));
+
+    // Quando o atraso terminar, define mostrarDiv como false
+    timer.subscribe(() => {
+      this.listaErros = [];
+    });
+  }
+
+  fecharModal(): void{
+    this.listaErros = [];
+  }
 
 }
