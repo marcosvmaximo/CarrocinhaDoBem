7872373cbf8d61cfe53b44808acdec16ac00d395
marcosvmaximo <marcosvinmaximo@gmail.com>
2024-04-13 19:52:00 -0300

    Ajustado navegação entre as páginas, roteamento, e guard

---
+++ src/app/pages/doacoes/doacoes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doacoes',
+  templateUrl: './doacoes.component.html',
+  styleUrl: './doacoes.component.css'
+})
+export class DoacoesComponent {
+
+}
