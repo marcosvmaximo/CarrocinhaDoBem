595d69b6ca90f977cd357bf89de0a11ec8bc69d7
vini-vg <vini02viana@gmail.com>
2024-04-15 17:13:18 -0300

    Implementação do formulario de edição

--- src/app/pages/perfil-edit/perfil-edit.component.ts
+++ src/app/pages/perfil-edit/perfil-edit.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-perfil-edit',
   templateUrl: './perfil-edit.component.html',
-  styleUrl: './perfil-edit.component.css'
+  styleUrls: ['./perfil-edit.component.css']
 })
 export class PerfilEditComponent {
+  perfilForm: FormGroup;
+  listaErros: string[] = [];
 
+  constructor(private formBuilder: FormBuilder) {
+    this.perfilForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(100), Validators.pattern("^[a-zA-ZÀ-ÖØ-öø-ÿ']+(\\s[a-zA-ZÀ-ÖØ-öø-ÿ']+)*$")]],
+      email: ['', [Validators.required, Validators.minLength(10), Validators.email, Validators.maxLength(100)]],
+      endereco: ['', [Validators.required, Validators.maxLength(100)]],
+      telefone: ['', [Validators.required, Validators.minLength(12), Validators.maxLength(100), Validators.pattern("/^(\d{2})\s(\d{5}|\d{4})-\d{4}$/")]],
+      cpf: ['', [Validators.required, Validators.maxLength(100), Validators.pattern("/^\d{3}\.\d{3}\.\d{3}-\d{2}$/")]]
+    });
+  }
+
+  onSubmit() {
+    this.listaErros = [];
+
+    Object.keys(this.perfilForm.controls).forEach(campo => {
+      const control = this.perfilForm.get(campo);
+
+      if (control && control.invalid && control.touched) {
+        const errors = control.errors as ValidationErrors; 
+
+        Object.keys(errors).forEach(erro => {
+          switch (erro) {
+            case 'required':
+              this.listaErros.push(`O campo ${campo} é obrigatório.`);
+              break;
+            case 'maxlength':
+              this.listaErros.push(`O campo ${campo} excedeu o número máximo de caracteres permitidos.`);
+              break;
+              case 'minlength':
+              this.listaErros.push(`O campo ${campo} deve ter no minimo 4 caracteres.`);
+              break;
+            case 'email':
+              this.listaErros.push(`O campo ${campo} deve ser um e-mail válido.`);
+              break;
+              
+          }
+        });
+      }
+    });
+  }
 }
